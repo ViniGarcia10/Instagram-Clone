a06e5b634296faaf89518efa50e19331d3280485
ViniGarcia10 <viniciusgarcia49@gmail.com>
2019-08-19 18:04:01 -0400

    insercao de assets e styles header

--- src/routes.js
+++ src/routes.js
@@ -1,10 +1,22 @@
+import React from 'react';
+import { Image } from 'react-native';
 import { createAppContainer, createStackNavigator } from 'react-navigation';
+
+import logo from './assets/instagram.png';
 
 import Feed from './Pages/Feed';
 
 const Routes = createAppContainer(
   createStackNavigator({
     Feed
+  }, {
+    headerLayoutPreset: 'center',
+    defaultNavigationOptions: {
+      headerTitle: <Image source={logo} />,
+      headerStyle: {
+        backgroudColor: '#f5f5f5'
+      }
+    },
   })
 );
 
